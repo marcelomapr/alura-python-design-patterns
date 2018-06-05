c10b2fe47454cdd7be68c64bb1146ecb21fcfb57
Aparecido de Paula Rosa <mora@gft.com>
2018-06-05 09:29:12 -0300

    versao final aula 1

--- aula1/calculador_de_impostos.py
+++ aula1/calculador_de_impostos.py
@@ -1,18 +1,15 @@
-# -*- coding: UTF-8 -*-
-
-# calculador_de_impostos.py
-
 class Calculador_de_impostos(object):
 
-  def realiza_calculo(self, orcamento):
-
-    icms_calculado = orcamento.valor * 0.1
-    print(icms_calculado)
+    def realiza_calculo(self, orcamento, imposto):
+        valor = imposto.calcula(orcamento)
+        print(valor)
 
 if __name__ == '__main__':
 
     from orcamento import Orcamento
+    from impostos import ISS, ICMS
 
     orcamento = Orcamento(500.0)
     calculador_de_impostos = Calculador_de_impostos()
-    calculador_de_impostos.realiza_calculo(orcamento) ## imprimirá 50.0
+    calculador_de_impostos.realiza_calculo(orcamento, ISS())
+    calculador_de_impostos.realiza_calculo(orcamento, ICMS())
